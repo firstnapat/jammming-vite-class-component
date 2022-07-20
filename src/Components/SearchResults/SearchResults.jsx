06213ef0918bb5c8ef84c6542c5f558c1281ab85
Napatsakorn Pianchana <napatdmsu@gmail.com>
2022-07-20 14:40:37 +0700

    Create Static Components

---
+++ src/Components/SearchResults/SearchResults.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { TrackList } from './TrackList';
+import './SearchResults.css';
+
+
+export class SearchResults extends Component {
+    render() {
+        return (
+            <div className="SearchResults">
+                <h2>Results</h2>
+                {/* <!-- Add a TrackList component --> */}
+            </div>
+
+        )
+    }
+};
